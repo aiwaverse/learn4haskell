f24fd41f777b0be663f7e4e9163446f88a7e7a32
Phi <aiwavision@protonmail.com>
2021-10-05 09:46:43 -0300

    Changes to chapter 1

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -492,7 +492,7 @@ Implement a function that returns the last digit of a given number.
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
 lastDigit :: Int -> Int
-lastDigit = (`mod` 10)
+lastDigit = (`mod` 10) . abs
 
 
 {- |
